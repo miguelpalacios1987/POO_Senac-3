f3f1f417109e5be675bdb0ef08fcc9987be9609d
miguelpalacios1987 <104658338+miguelpalacios1987@users.noreply.github.com>
2023-01-31 17:51:26 -0300

    Emcapsulamento

---
+++ Aula06.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Emcapsulamento;
+
+/**
+ *
+ * @author Miguel Palacios
+ */
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolumen();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
